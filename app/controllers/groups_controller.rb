11c41a6be0687f8e24a6fac908c6c3223710b465
s5346 <billliu@billliutekiMacBook-Air.local>
2014-04-03 17:11:11 +0800

    Ctrate done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,12 +7,17 @@ class GroupsController < ApplicationController
 	end
 
 	def new
+		@group = Group.new
 	end
 
 	def edit
 	end
 
 	def create
+		@group = Group.new(group_params)
+		@group.save
+
+		redirect_to group_path
 	end
 
 	def update
@@ -20,4 +25,10 @@ class GroupsController < ApplicationController
 
 	def destroy
 	end 
+
+	private
+
+	def group_params
+		params.require(:group).permit(:title, :description)
+	end
 end
